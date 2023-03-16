796fbff8235b46b92f0329251d9510cfa13c71a1
cjsolis <cjelizondo12@gmail.com>
2023-03-16 17:22:39 -0500

    added create element for weather icon in js

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 var userFormEl = document.querySelector('#user-form')
 var citySearchEl = document.querySelector('#city-search')
 var searchHistoryList = document.querySelector('#search-history-list')
+var mainWeatherTitle = document.querySelector('.main-weather-title')
 
 var formSubmitHandler = function (event){
     event.preventDefault();
@@ -14,7 +15,7 @@ var formSubmitHandler = function (event){
         searchedCity.innerText = city;
         searchHistoryList.append(searchedCity)
         citySearchEl.value = ''
-
+    displayWeather()
 }
 }
 
@@ -28,19 +29,32 @@ const uvData = function(lat,lon){
 
 
 var getCity = function (cityName){
-   var apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=d9ec41326a5c9cfd94ea3263abf80c6c`
-    fetch(apiUrl)
-        .then(response =>{
-            return response.json()
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d9ec41326a5c9cfd94ea3263abf80c6c`)
+        .then(res =>{
+            return res.json()  
            }).then(data=>{
             console.log(data)
             uvData(data.coord.lat,data.coord.lon)
+            displayWeather(data)
            })
  };
 
-// var displayWeather = function (city){
+var displayWeather = function (data){
+    if(data) {
+        var tempK = data.main.temp;
+        var tempF = Math.floor((tempK - 273.15) * 1.8 + 32);
+        console.log(tempK)
+        console.log(tempF)
+    document.querySelector('.current-weather-title').innerHTML = `${data.name}`
+    var weatherIcon = document.createElement('img')
+    weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    mainWeatherTitle.append(weatherIcon)
+    document.querySelector('.temp').innerHTML = `Temp: ${tempF} &#x2109;`
+    document.querySelector('.wind').innerHTML = `Wind: ${data.wind.speed}`
+    document.querySelector('.humidity').innerHTML = `Humidity: ${data.main.humidity}%`
+    }
 
-// }
+}
 
 
 
