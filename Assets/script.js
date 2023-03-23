6a0c8a5b3fe1584c9a784ba8a884ab1df1290320
cjsolis <cjelizondo12@gmail.com>
2023-03-23 16:18:45 -0500

    added README file

--- Assets/script.js
+++ Assets/script.js
@@ -3,40 +3,39 @@ var citySearchEl = document.querySelector("#city-search");
 var searchHistoryList = document.querySelector("#search-history-list");
 var mainWeatherTitle = document.querySelector(".main-weather-title");
 var clearBtn = document.querySelector("#clear-btn");
-var allSearchedCities = []
+var allSearchedCities = [];
 
-
-// Function when search is clicked 
+// Function when search is clicked
 var formSubmitHandler = function (event) {
   event.preventDefault();
-  var city = citySearchEl.value.trim()
- 
+  var city = citySearchEl.value.trim();
+
   if (city && !allSearchedCities.includes(city)) {
     getCity(city);
     var searchedCity = document.createElement("button");
-    searchedCity.setAttribute("class", "cities")
+    searchedCity.setAttribute("class", "cities");
     searchedCity.innerText = city;
     searchHistoryList.append(searchedCity);
     citySearchEl.value = "";
-    allSearchedCities.push(city)
-    localStorage.setItem('searchHistory', JSON.stringify(allSearchedCities))
-  } 
+    allSearchedCities.push(city);
+    localStorage.setItem("searchHistory", JSON.stringify(allSearchedCities));
+  }
 };
 
 // New button for each city searched on aside bar
-var searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
-if(searchHistory){
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+if (searchHistory) {
   allSearchedCities = searchHistory;
-  searchHistory.forEach(function(city){
+  searchHistory.forEach(function (city) {
     var searchedCity = document.createElement("button");
-    searchedCity.setAttribute("class", "cities")
+    searchedCity.setAttribute("class", "cities");
     searchedCity.innerText = city;
-    searchHistoryList.append(searchedCity)
-});
+    searchHistoryList.append(searchedCity);
+  });
 }
 
-// When button is clicked, city weather is displayed 
-searchHistoryList.addEventListener("click", function(event) {
+// When button is clicked, city weather is displayed
+searchHistoryList.addEventListener("click", function (event) {
   if (event.target.matches("button")) {
     var city = event.target.innerText;
     getCity(city);
@@ -44,12 +43,12 @@ searchHistoryList.addEventListener("click", function(event) {
 });
 
 // Search History Clear button
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener("click", () => {
   searchHistoryList.innerHTML = "";
-  localStorage.removeItem('searchHistory');
-})
+  localStorage.removeItem("searchHistory");
+});
 
-// API call for 5 day array 
+// API call for 5 day array
 var fiveDayWeather = function (lat, lon) {
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=d9ec41326a5c9cfd94ea3263abf80c6c`
@@ -77,35 +76,36 @@ var getCity = function (cityName) {
       fiveDayWeather(data.coord.lat, data.coord.lon);
       showFiveDays(data);
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
 };
 
 var displayCurrentWeather = function (data) {
   if (data) {
     // var date = new Date(data.list[0].dt * 1000);
-        var city = data.name
-        var temperature = Math.round((data.main.temp - 273.15) * 1.8 + 32);
-        var humid = data.main.humidity;
-        var wind = data.wind.speed
-        var icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-        var date = new Date().toLocaleDateString()
-        var html = `<div class="col-md">
+    var city = data.name;
+    var temperature = Math.round((data.main.temp - 273.15) * 1.8 + 32);
+    var humid = data.main.humidity;
+    var wind = data.wind.speed;
+    var icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    var date = new Date().toLocaleDateString();
+    var html = `<div class="col-md">
                         <h2 class="card-title text-primary">${city}</h2>
                         <p>${date}</p>
                         <img src="${icon}" alt="weather icon">
                         <p class="card-text">Temp: ${temperature}°F</p>
                         <p class="card-text">Wind: ${wind}%</p>
                         <p class="card-text">Humidity: ${humid}%</p>
-                     </div>`}
-        let row = document.querySelector('.current-weather-box')
-        row.innerHTML = html;
+                     </div>`;
+  }
+  let row = document.querySelector(".current-weather-box");
+  row.innerHTML = html;
 };
 
 var showFiveDays = function (data) {
   var forecastDays = [];
   var today = new Date();
-  var tomorrow = new Date(today)
-  tomorrow.setDate(tomorrow.getDate() + 1)
+  var tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
   // var date = new Date(data.list[2].dt * 1000);
 
   for (let i = 0; i < 5; i++) {
@@ -117,7 +117,7 @@ var showFiveDays = function (data) {
     if (forecast) {
       var temperature = Math.round((forecast.main.temp - 273.15) * 1.8 + 32);
       var humid = forecast.main.humidity;
-      var wind = forecast.wind.speed
+      var wind = forecast.wind.speed;
       var icon = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
 
       forecastDays.push(
@@ -132,12 +132,13 @@ var showFiveDays = function (data) {
             </div>
           </div>
         </div>
-      `);
+      `
+      );
     }
-    tomorrow.setDate(tomorrow.getDate()+1);
+    tomorrow.setDate(tomorrow.getDate() + 1);
   }
-  let row = document.querySelector('.weather.row')
-    row.innerHTML = forecastDays.join('')
+  let row = document.querySelector(".weather.row");
+  row.innerHTML = forecastDays.join("");
 };
 
 // When user submits city
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Weather-App
+
+
+## Description
+This is a weather app using the api https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} to get the current weather. User can search a city and should be displayed the current weather as well as the consecutive 5 days. I was able to practice my skills calling an api using the fetch function.
+
+## Installation
+- copy github https/ssh link
+- git clone to terminal with github https/ssh link
+
+## Usage
+
+![Wepage screenshot](weatherAppScreenshot.png)
+
+https://cjsolis12.github.io/Weather-App/
+
+## Credits
+- TA Zachariah Shone
+- https://www.youtube.com/watch?v=nGVoHEZojiQ
+- https://stackoverflow.com/
+- https://openweathermap.org/forecast5
+- https://www.javascripttutorial.net/javascript-dom/javascript-append/
